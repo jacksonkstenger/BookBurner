2a22ce4dbd796ba196325048214ca0b697634d14
Grant Stenger <gstenger98@gmail.com>
2021-08-24 00:20:59 -0700

    organizing files

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
+# Import Dependencies
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.base.exceptions import TwilioRestException
-from flask import Flask, request, redirect
-import sys
-import os
-
-from utils import send_text
-from logic import logic
-from title_to_link import title_to_link
-
-# Import Dependencies
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from flask import Flask, request, redirect
 import urllib.parse
 import time
+import sys
 import os
+sys.path.append('scripts/')
+
+from utils import send_text
+from logic import logic
+from title_to_link import title_to_link
 
 app = Flask(__name__)
 
