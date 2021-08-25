9b4f0036c7fbefa76b5cf727c2e755353401ad59
jacksonkstenger <jacksonkstenger@gmail.com>
2021-08-24 20:19:23 -0700

    barcode method added

---
+++ scripts/image_to_title_barcode.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+import sys
+import cv2
+from pyzbar.pyzbar import decode
+import re
+sys.path.append('../data/')
+
+def BarcodeReader(book_url):
+    img = cv2.imread(book_url)
+    detectedBarcodes = decode(img)
+    if not detectedBarcodes:
+        print("Error")
+    else:
+        for barcode in detectedBarcodes:
+            if barcode.data != "":
+                result = barcode.data
+                return result.decode()
+    return None
+
+def BarcodeLookup(barcode):
+    DRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH', None)
+    #DRIVER_PATH = r"C:\Users\Jackson\Downloads\chromedriver_win32\chromedriver.exe"
+    driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+    url = 'https://www.barcodelookup.com/' + barcode
+    driver.get(url)
+    element = driver.find_element_by_xpath('/html/body/div[1]/section[2]/div/div/div[2]')
+    result = element.text
+    driver.close()
+    title = result.split('\n')[1]
+    author = re.findall('\nAuthor.*\n',result)[0].replace('\n','')[8:]
+    final_result = re.sub(r'\W+', ' ', title + ' ' + author)
+    return final_result
+    
+def image_to_title(image_url):
+    barcode = BarcodeReader(image_url)
+    information = BarcodeLookup(barcode)
+    return information
+
+
+if __name__ == "__main__":
+    # url = "https://api.twilio.com/2010-04-01/Accounts/AC15abe67bd635f83f4fa678d17ccf9e6d/Messages/MM2731b3d74b95937e4caea55f86530858/Media/ME0b75de784bb7dd2b33d4e97ddc156ccd"
+    url = "https://api.twilio.com/2010-04-01/Accounts/AC15abe67bd635f83f4fa678d17ccf9e6d/Messages/MM5da7bbd6c8af667b494b94d1dfb8c215/Media/ME828bbe7ea2257e7dbb0b370a7eba1908"
+    print(image_to_title(url))
