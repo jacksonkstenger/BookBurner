9421fb220f8d3a5f56388eaf591dacfbc526af21
jacksonkstenger <jacksonkstenger@gmail.com>
2021-08-25 19:45:01 -0700

    forgot to add imports to barcode

--- scripts/image_to_title_barcode.py
+++ scripts/image_to_title_barcode.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import time
 import os
 import sys
