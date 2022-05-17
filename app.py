236da24766e3ee66c6349eb69c07e4037a074a52
jacksonkstenger <jacksonkstenger@gmail.com>
2022-05-16 22:33:00 -0500

    swapping out send_text to see if it works better

--- app.py
+++ app.py
@@ -2,16 +2,16 @@
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.base.exceptions import TwilioRestException
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-import urllib.parse
-import time
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.by import By
+# import urllib.parse
+# import time
 import sys
-import os
+# import os
 sys.path.append('scripts/')
 
 from utils import send_text
@@ -54,7 +54,8 @@ def sms_reply():
                 text = image_to_title_og(media_url)
         
         # Add a message
-        resp.message("Time to burn some books! Here's a free version of {}.".format(text))
+        ##resp.message("Time to burn some books! Here's a free version of {}.".format(text))
+        send_text("Time to burn some books! Here's a free version of {}.".format(text), from_number)
 
         # Get a link to the pdf of this book
         url = title_to_link(text)#title_to_link_old(text)#title_to_link(text)
@@ -67,18 +68,23 @@ def sms_reply():
             audio_url == ""
         #if (url == "") and (audio_url == ""):
         if (not url) and (not audio_url):
-            resp.message("I can't find a pdf of this title. Try including more details.")
+            #resp.message("I can't find a pdf of this title. Try including more details.")
+            send_text("I can't find a pdf of this title. Try including more details.", from_number)
         elif (not not url) and (not audio_url):
-            resp.message("{}".format(url))
+            #resp.message("{}".format(url))
+            send_text("{}".format(url), from_number)
         elif (not url) and (not not audio_url):
-            resp.message(rf"I can't find a pdf of this title. I did find this link, it may be an audio book: {audio_url}")
+            #resp.message(rf"I can't find a pdf of this title. I did find this link, it may be an audio book: {audio_url}")
+            send_text(rf"I can't find a pdf of this title. I did find this link, it may be an audio book: {audio_url}", from_number)
         else:
-            resp.message(rf"{url} (possible audio version: {audio_url})")
+            #resp.message(rf"{url} (possible audio version: {audio_url})")
+            send_text(rf"{url} (possible audio version: {audio_url})", from_number)
         return str(resp)
 
     except TwilioRestException as e:
         print(e)
-        resp.message("I can't find a pdf of this title.")
+        #resp.message("I can't find a pdf of this title.")
+        send_text("I can't find a pdf of this title.", from_number)
         return str(resp)
 
 if __name__ == "__main__":
